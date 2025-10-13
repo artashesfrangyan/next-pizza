9355d9ca9a45f2ae18b7e76f6f738acfe3ecf3ec
artashesfrangyan <frangyan99@gmail.com>
2025-10-13 08:34:17 +0300

    updated prisma schema

---
+++ prisma/seed.ts
@@ -0,0 +1,36 @@
+import { hashSync } from "bcrypt";
+import { prisma } from "./prisma-client";
+
+async function up() {
+  await prisma.user.createMany({
+    data: [
+      {
+        fullName: "User",
+        email: "user@test.ru",
+        password: hashSync("111111", 10),
+        verified: new Date(),
+        role: "USER",
+      },
+      {
+        fullName: "Admin",
+        email: "admin@test.ru",
+        password: hashSync("111111", 10),
+        verified: new Date(),
+        role: "ADMIN",
+      },
+    ],
+  });
+}
+
+async function down() {
+  await prisma.$executeRaw`TRUNCATE TABLE "User" RESTART IDENTITY CASCADE`;
+}
+
+async function main() {
+  try {
+    await down();
+    await up();
+  } catch (e) {
+    console.error(e);
+  }
+}
