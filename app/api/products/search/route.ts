b05df240d63d06a04d2ded63f14e17ec84137680
artashesfrangyan <frangyan99@gmail.com>
2025-11-18 13:09:18 +0300

    created a search

---
+++ app/api/products/search/route.ts
@@ -0,0 +1,22 @@
+import { prisma } from "@/prisma/prisma-client";
+import { NextRequest, NextResponse } from "next/server";
+
+export async function GET(req: NextRequest) {
+  const query = req.nextUrl.searchParams.get("query");
+
+  if (!query) {
+    return NextResponse.json({ error: "Query is required" }, { status: 400 });
+  }
+
+  const product = await prisma.product.findMany({
+    where: {
+      name: {
+        contains: query,
+        mode: "insensitive",
+      },
+    },
+    take: 5,
+  });
+
+  return NextResponse.json(product);
+}
