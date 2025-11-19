565659c27069533d84cff63bac223db286c04b5e
artashesfrangyan <frangyan99@gmail.com>
2025-11-19 13:06:57 +0300

    added debouncing for a search query

--- components/shared/search-input.tsx
+++ components/shared/search-input.tsx
@@ -6,8 +6,8 @@ import { Product } from "@prisma/client";
 import { Search } from "lucide-react";
 import Image from "next/image";
 import Link from "next/link";
-import { useEffect, useRef, useState, type FC } from "react";
-import { useClickAway } from "react-use";
+import { useRef, useState, type FC } from "react";
+import { useClickAway, useDebounce } from "react-use";
 
 interface Props {
   className?: string;
@@ -19,9 +19,13 @@ export const SearchInput: FC<Props> = ({ className }) => {
   const [products, setProducts] = useState<Product[]>([]);
   const ref = useRef<HTMLInputElement>(null);
 
-  useEffect(() => {
-    Api.products.search(searchQuery).then((res) => setProducts(res));
-  }, [searchQuery]);
+  useDebounce(() => {
+    if (!searchQuery) {
+      setProducts([]);
+    } else {
+      Api.products.search(searchQuery).then((res) => setProducts(res));
+    }
+  }, 250, [searchQuery]);
 
   useClickAway(ref, () => {
     setFocused(false);
@@ -50,6 +54,7 @@ export const SearchInput: FC<Props> = ({ className }) => {
           onChange={(e) => setSearchQuery(e.target.value)}
         />
 
+        {products.length > 0 &&(          
         <div
           className={cn(
             "absolute w-full bg-white rounded-xl py-2 top-14 shadow-md transition-all duration-200 invisible opacity-0 z-30",
@@ -62,7 +67,7 @@ export const SearchInput: FC<Props> = ({ className }) => {
               href={`/products/${product.id}`}
               className="flex items-center gap-3 w-full px-3 py-2 hover:bg-primary/10"
             >
-              <div className="h-8 w-8">
+              <div className="h-8 w-8 relative">
                 <Image
                   className="rounded-sm"
                   fill
@@ -75,6 +80,7 @@ export const SearchInput: FC<Props> = ({ className }) => {
             </Link>
           ))}
         </div>
+        )}
       </div>
     </>
   );
